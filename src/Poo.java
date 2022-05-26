d6610c47fc11811b57ae8fb1efdc776dec5417c1
JuanitaCampos1505 <88303473+JuanitaCampos1505@users.noreply.github.com>
2022-05-25 23:14:06 -0500

    COPYRIGHT METHOD

--- src/Poo.java
+++ src/Poo.java
@@ -4,7 +4,15 @@ public class Poo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+    
+	}
+	
+	public static void DerechosAutor() {
+		System.out.println("© 2022 POO Project. Todos los derechos reservados. "
+				+          "El uso de nuestro Software implica que usted acepta nuestros termino de uso "
+				+          "y nuestras políticas de privacidad");
+		
 	}
 
 }
+
